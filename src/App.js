aaa8f9e891222a49078927fba81ad3518c6c4287
Amir Harandi <a.eslami76@gmail.com>
2018-12-26 00:44:20 +0000

    Show user's latitude

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    lat: null,
+    errorMessage: ''
+  };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        const {
+          coords: { latitude: lat }
+        } = position;
+        this.setState({ lat });
+      },
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
   render() {
-    return <div>React Seasons</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading</div>;
   }
 }
 
